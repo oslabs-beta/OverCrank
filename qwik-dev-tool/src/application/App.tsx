e6fcb29b2f0606ffd2f03b80122eb28e25bc57a6
Paul Sturtz <sturtzpaul@gmail.com>
2023-04-18 11:07:53 -0400

    App tweaks

--- qwik-dev-tool/src/application/App.tsx
+++ qwik-dev-tool/src/application/App.tsx
@@ -2,25 +2,23 @@ import React, {useState, useEffect} from 'react';
 import networkListener from './algorithms/networkListener';
 import buildTree from './algorithms/parseHtml';
 import testDOM from './algorithms/getHtmlTest';
-import { elementInfo } from './types/types';
 import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { NodeData, Links } from './types/types';
 
 
-const tree: elementInfo[] = buildTree(testDOM);
-
 
 const App = () => {
   const [nodeData, setNodeData] = useState<NodeData>({})
-  const [tree, setTree] = useState<JSX.Element>()
+  const [tree, setTree] = useState<JSX.Element | null>(null)
   const [unassigned, setUnassigned] = useState<Links>({})
-  const [currentNode, setCurrentNode] = useState<number>()
+  const [currentNode, setCurrentNode] = useState<number>(0)
 
   useEffect(() => networkListener(), []) // feed network listener nodeData
   useEffect(() => {
     // call dom parser
+    
   }, [nodeData])
 
 
@@ -33,7 +31,7 @@ const App = () => {
       defaultExpandIcon={<ChevronRightIcon />}
       sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
       >
-        {tree[tree.length - 1].element}
+        {tree}
       </TreeView>
     </>
     
